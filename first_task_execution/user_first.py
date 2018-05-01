eb17af5430a91d946ac88bf18d0ad1e0a26f5165
thekingofcity <1634814279@qq.com>
2018-05-01 16:41:06 +0800

    execute_extend_user_task

--- first_task_execution/user_first.py
+++ first_task_execution/user_first.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append('.')
 sys.path.append('..')
 
-from tasks import (execute_user_task, execute_extend_user_task)
+from tasks.user import (execute_user_task, execute_extend_user_task)
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
