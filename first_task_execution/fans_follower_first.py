3eed987bbe07a65f94b0ae0fe7d543955a3b8132
thekingofcity <wzl7777777@gmail.com>
2018-05-09 16:16:33 +0800

    batch fans_followers

--- first_task_execution/fans_follower_first.py
+++ first_task_execution/fans_follower_first.py
@@ -1,5 +1,7 @@
 import sys
 
+from res import uid2name__
+
 sys.path.append('.')
 sys.path.append('..')
 
@@ -11,6 +13,9 @@ if __name__ == '__main__':
             execute_followers_fans_task(int(sys.argv[1]), int(sys.argv[2]))
         else:
             print("Please input standard uid.")
+    elif len(sys.argv) == 1:
+        for uid in uid2name__:
+            execute_followers_fans_task(uid, uid2name__[uid][1])
     elif len(sys.argv) < 3:
         print("Please specific an uid and a verify_type.")
     else:
