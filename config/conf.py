6ebd439d14d252ed3c382eb23e509878c9d0a3ff
thekingofcity <wzl7777777@gmail.com>
2020-01-27 11:28:05 +0800

    split config file

--- config/conf.py
+++ config/conf.py
@@ -5,11 +5,18 @@ from pathlib import Path
 from yaml import load
 
 config_path = os.path.join(os.path.dirname(__file__), 'spider.yaml')
+instance_config_path = os.path.join(os.path.dirname(__file__), 'instance.yaml')
 
 with open(config_path, encoding='utf-8') as f:
     cont = f.read()
-
 cf = load(cont)
+
+instance_specfic = False
+if os.path.exists(instance_config_path):
+    instance_specfic = True
+    with open(instance_config_path, encoding='utf-8') as f:
+        instance_cont = f.read()
+    instance_cf = load(instance_cont)
 
 
 def get_db_args():
@@ -25,7 +32,8 @@ def get_timeout():
 
 
 def get_crawl_interal():
-    interal = random.randint(cf.get('min_crawl_interal'), cf.get('max_crawl_interal'))
+    interal = random.randint(cf.get('min_crawl_interal'),
+                             cf.get('max_crawl_interal'))
     return interal
 
 
@@ -63,15 +71,17 @@ def get_broker_and_backend():
     sentinel_args = redis_info.get('sentinel', '')
     db = redis_info.get('broker', 5)
     if sentinel_args:
-        broker_url = ";".join('sentinel://:{}@{}:{}/{}'.format(password, sentinel['host'], sentinel['port'], db) for
-                              sentinel in sentinel_args)
+        broker_url = ";".join('sentinel://:{}@{}:{}/{}'.format(
+            password, sentinel['host'], sentinel['port'], db)
+                              for sentinel in sentinel_args)
         return broker_url
     else:
         host = redis_info.get('host')
         port = redis_info.get('port')
         backend_db = redis_info.get('backend', 6)
         broker_url = 'redis://:{}@{}:{}/{}'.format(password, host, port, db)
-        backend_url = 'redis://:{}@{}:{}/{}'.format(password, host, port, backend_db)
+        backend_url = 'redis://:{}@{}:{}/{}'.format(password, host, port,
+                                                    backend_db)
         return broker_url, backend_url
 
 
@@ -120,7 +130,8 @@ def get_images_allow():
 
 
 def get_images_path():
-    img_dir = cf.get('images_path') if cf.get('images_path') else os.path.join(str(Path.home()), 'weibospider', 'images')
+    img_dir = cf.get('images_path') if cf.get('images_path') else os.path.join(
+        str(Path.home()), 'weibospider', 'images')
     if not os.path.exists(img_dir):
         os.makedirs(img_dir)
     return img_dir
@@ -147,4 +158,6 @@ def get_user_info_expire_time():
 
 
 def get_adapter_ip():
-    return cf.get('adapter_ip')
+    if instance_specfic:
+        return instance_cf.get('adapter_ip')
+    return None
